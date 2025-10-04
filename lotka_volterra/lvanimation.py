fc068c7e14e66c61554b7f4f12f43f076c65f940
Jana <janadahlmanns@gmail.com>
2025-10-04 12:46:54 +0200

    Animated the animals

--- lotka_volterra/lvanimation.py
+++ lotka_volterra/lvanimation.py
@@ -45,20 +45,6 @@ class LVAnimation(MovingCameraScene):
         )
         self.add(axes, labels)
 
-        # rabbit curve
-        rabbit_curve = axes.plot_line_graph(
-            x_values=t, y_values=R,
-            line_color="#0072B2",
-            add_vertex_dots=False
-        ).set_z_index(2)
-
-        # fox curve
-        fox_curve = axes.plot_line_graph(
-            x_values=t, y_values=F,
-            line_color="#E69F00",
-            add_vertex_dots=False
-        ).set_z_index(2)
-        
         # --- Forest Panel on the Left ---
         forest_radius = 2.2
         forest_panel_size = 5  # width and height of clearing
@@ -72,11 +58,29 @@ class LVAnimation(MovingCameraScene):
             "lotka_volterra/tree2_green.svg": [74, 250, 320],
             "lotka_volterra/tree1_lightgreen.svg": [270, 315],
             "lotka_volterra/tree2_darkgreen.svg": [80, 200, 340],
-            "lotka_volterra/tree1_green.svg": [135, 160, 260],
+            "lotka_volterra/tree1_green.svg": [135, 180, 260],
             "lotka_volterra/tree1_darkgreen.svg": [150, 10, 20],
         }
-
         trees = VGroup()
+
+        # add some more trees outside the clearing
+        outer_trees = VGroup()
+        additional_trees = [[3,1.2,50], [4, 1.4,45], [2, 1.1,45], [5,1.2,40], [0,1.2,125], [1,1.4,135],[4,1.2,140], [3,1,110],
+                            [5,1.4,225], [0,1.1,240], [1,1.15,215], [3,1.1,300], [4,1.15,310], [0,1.4,315]]
+        tree_types = [
+            "lotka_volterra/tree1_green.svg",
+            "lotka_volterra/tree1_lightgreen.svg",
+            "lotka_volterra/tree1_darkgreen.svg",
+            "lotka_volterra/tree2_green.svg",
+            "lotka_volterra/tree2_lightgreen.svg",
+            "lotka_volterra/tree2_darkgreen.svg"]
+        for additional_tree in additional_trees:
+            tree = SVGMobject(tree_types[additional_tree[0]]).scale(0.3)
+            pos = forest_clearing.get_center() + (forest_radius * additional_tree[1]) * np.array([np.cos(additional_tree[2]*DEGREES), np.sin(additional_tree[2]*DEGREES), 0])
+            tree.move_to(pos)
+            trees.add(tree)
+
+        inner_trees = VGroup()
         for svg_path, angles in tree_map.items():
             for angle_deg in angles:
                 angle_rad = angle_deg * DEGREES
@@ -84,25 +88,79 @@ class LVAnimation(MovingCameraScene):
                 pos = forest_clearing.get_center() + forest_radius * np.array([np.cos(angle_rad), np.sin(angle_rad), 0])
                 tree.move_to(pos)
                 trees.add(tree)
+        
 
         # Rabbits map
         rabbits_map = [[0,0],[0.8,0], [0.5,90], [0.7,40], [0.3,160], [0.6, 200],[0.8, 280],[0.3,320],[0.8,140],[0.5,240]]
         rabbit_svg = "lotka_volterra/rabbit.svg"
+        n_rabbits = len(rabbits_map)
 
         rabbits = VGroup()
         for rabbit_pos in rabbits_map:
             rabbit = SVGMobject(rabbit_svg ).scale(0.3)
             pos = forest_clearing.get_center() + (rabbit_pos[0]*forest_radius) * np.array([np.cos(rabbit_pos[1]*DEGREES), np.sin(rabbit_pos[1]*DEGREES), 0])
             rabbit.move_to(pos)
+            rabbit.set_opacity(0)
             rabbits.add(rabbit)
 
-        forest_panel = VGroup(forest_clearing, trees, rabbits)
-        forest_panel.move_to(np.array([-8, 0, 0]))
+        # Foxes map
+        foxes_map = [[0.7,220],[0.5,0],[0.6,60], [0.4,300]]
+        fox_svg = "lotka_volterra/fox.svg"
+        n_foxes = len(foxes_map)
 
+        foxes = VGroup()
+        for fox_pos in foxes_map:
+            fox = SVGMobject(fox_svg ).scale(0.2)
+            pos = forest_clearing.get_center() + (fox_pos[0]*forest_radius) * np.array([np.cos(fox_pos[1]*DEGREES), np.sin(fox_pos[1]*DEGREES), 0])
+            fox.move_to(pos)
+            fox.set_opacity(0)
+            foxes.add(fox)
+
+        forest_panel = VGroup(forest_clearing, trees, rabbits, foxes)
+        forest_panel.move_to(np.array([-8, 0, 0]))
         self.add(forest_panel)
 
+        # Create initial plot curves
+        rabbit_curve = VMobject(color="#0072B2").set_z_index(2)
+        fox_curve = VMobject(color="#E69F00").set_z_index(2)
+        self.add(rabbit_curve, fox_curve)
 
-        # animate drawing
-        self.play(Create(rabbit_curve, run_time=10),
-                  Create(fox_curve, run_time=10))
+        for t_current in range(1,len(t)): # advance time
+            # rabbit curve
+            rabbit_curve.become(axes.plot_line_graph(
+                x_values=t[:t_current], y_values=R[:t_current],
+                line_color="#0072B2",
+                add_vertex_dots=False
+            ).set_z_index(2))
+
+            # fox curve
+            fox_curve.become(axes.plot_line_graph(
+                x_values=t[:t_current], y_values=F[:t_current],
+                line_color="#E69F00",
+                add_vertex_dots=False
+            ).set_z_index(2))
+       
+            # calculate rabbit opacities
+            def get_opacities(population, n_animals):
+                full = int(np.floor(population))
+                partial = population - full
+                opacities = []
+                for i in range(n_animals):
+                    if i < full:
+                        opacities.append(1)
+                    elif i == full:
+                        opacities.append(partial)
+                    else:
+                        opacities.append(0)
+                return opacities
+
+            rabbit_opacities = get_opacities(R[t_current], n_rabbits)
+            fox_opacities = get_opacities(F[t_current], n_foxes)
+            for j, rabbit in enumerate(rabbits):
+                rabbit.set_opacity(rabbit_opacities[j])
+            for f, fox in enumerate(foxes):
+                fox.set_opacity(fox_opacities[f])
+
+            self.wait(0.1)
+
         self.wait(2)
