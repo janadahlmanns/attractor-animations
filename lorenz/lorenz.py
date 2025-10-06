ca3343ca9ed83a4842a3b01163604e5d72b2f025
janadahlmanns@gmail.com <janadahlmanns@gmail.com>
2025-10-06 17:51:41 +0200

    Added Lorenz animation

---
+++ lorenz/lorenz.py
@@ -0,0 +1,78 @@
+from manim import *
+import numpy as np
+from scipy.integrate import solve_ivp
+
+class Lorenz(ThreeDScene):
+    def lorenz(self, t, state, sigma=10, rho=28, beta=8/3):
+        x, y, z = state
+        dx = sigma * (y - x)
+        dy = x * (rho - z) - y
+        dz = x * y - beta * z
+        return [dx, dy, dz]
+
+    def construct(self):
+        # --- Simulation parameters ---
+        sigma, rho, beta = 10, 28, 8/3
+        t_span = (0, 30)             # total time span
+        t_eval = np.linspace(*t_span, 2000)  # time steps for smoothness, 5000 is pretty smooth
+        ball_runtime = 30
+
+        # Two slightly different initial conditions
+        init1 = [1.0, 1.0, 1.0]
+        init2 = [1.1, 1.1, 0.9]
+
+        # Solve Lorenz equations
+        sol1 = solve_ivp(lambda t, y: self.lorenz(t, y, sigma, rho, beta),
+                         t_span, init1, t_eval=t_eval)
+        sol2 = solve_ivp(lambda t, y: self.lorenz(t, y, sigma, rho, beta),
+                         t_span, init2, t_eval=t_eval)
+
+        # --- 3D Axes ---
+        axes = ThreeDAxes(
+            x_range=[-50, 50, 5],
+            y_range=[-50, 50, 5],
+            z_range=[-50, 50, 5],
+            x_length=10, y_length=10, z_length=10
+        )
+        axes.add(axes.get_axis_labels(x_label="x", y_label="y", z_label="z"))
+        self.add(axes)
+        self.set_camera_orientation(phi=60 * DEGREES, theta=-60 * DEGREES, zoom=0.8)
+
+
+
+        # --- Full attractor curves (faint for context) ---
+        # connect points to axes coordinate system
+        points1 = [axes.c2p(x, y, z) for x, y, z in zip(*sol1.y)]
+        points2 = [axes.c2p(x, y, z) for x, y, z in zip(*sol2.y)]
+        # draw curves
+        full_curve1 = VMobject(color=BLUE, stroke_opacity=1)
+        full_curve1.set_points_as_corners(points1)
+        full_curve2 = VMobject(color=ORANGE, stroke_opacity=1)
+        full_curve2.set_points_as_corners(points2)
+        # add curves
+        self.add(full_curve1, full_curve2)
+
+        # --- Moving dots ---
+        dot1 = Dot3D(points1[0], color=BLUE)
+        dot2 = Dot3D(points2[0], color=ORANGE)
+
+        # --- Trails (actual paths traced) ---
+        trail1 = VMobject(color=BLUE)
+        trail1.set_points_as_corners([points1[0]])
+        trail2 = VMobject(color=ORANGE)
+        trail2.set_points_as_corners([points2[0]])
+
+        self.add(dot1, dot2, trail1, trail2)
+
+        # --- Animate the two trajectories ---
+        self.begin_ambient_camera_rotation(rate=-(2*PI/(ball_runtime+2.5))) # rotate camera during the animation
+        self.wait(0.5) # to trigger rotation start
+        self.play(
+            MoveAlongPath(dot1, full_curve1),
+            MoveAlongPath(dot2, full_curve2),
+            run_time=ball_runtime,
+            rate_func=linear
+        )
+
+        self.wait(2)
+
